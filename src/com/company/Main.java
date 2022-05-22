87b774e0f72493e955f07f12c03e70b429efa883
Vasily Gudin <gudin@list.ru>
2022-05-22 11:18:38 +0300

    Залито в Git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,14 +2,14 @@ package com.company;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
         File dir = new File("Z://Games//savegame");
         if (dir.mkdir()) System.out.println();
@@ -35,12 +35,18 @@ public class Main {
         System.out.println("Создан архив " + archive.getName());
         for (File file : dir.listFiles()) {
             if (file.getName().contains(".dat")) {
-                zipFiles(String.valueOf(archive), file.getPath());
-                file.delete(); //TODO этот метод не работает (не удаляет)
-                System.out.println("Удален файл " + file.getName());
+                try {
+                    zipFiles(String.valueOf(archive), Arrays.asList(dir.list()));
+                    file.delete();
+                    System.out.println("Удален файл " + file.getName());
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
             }
         }
+
     }
+
 
     private static void saveGame(String way, GameProgress gameProgress) {
 
@@ -53,16 +59,23 @@ public class Main {
         }
     }
 
-    private static void zipFiles(String archive, String fileToArchive) {
+    private static void zipFiles(String archive, List<String> files) throws IOException {
         try {
             ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(archive)); //создаем объект архива
-            FileInputStream fis = new FileInputStream(fileToArchive);
-            zip.putNextEntry(new ZipEntry(fileToArchive)); //кладем в объект архив файлы, которые надо заархивировать
-            byte[] buffer = new byte[fis.available()]; fis.read(buffer);
-            zip.write(buffer);
-            zip.closeEntry();
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            for (String s : files) {
+                try {
+                    FileInputStream fis = new FileInputStream(s);
+                    zip.putNextEntry(new ZipEntry(new File(s).getName()));
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zip.write(buffer);
+                    zip.closeEntry();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
